6b842b6d2dd6dc17e2cc3ebc218b1cc8d185a5df
TomReilly1 <reilly.thomas.c@gmail.com>
2022-12-30 11:54:34 -0500

    test for valid first_name and last_name of Person model

--- AddressBook/test/models/person_test.rb
+++ AddressBook/test/models/person_test.rb
@@ -4,4 +4,8 @@ class PersonTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save person without first name and last name" do
+    person = Person.new
+    assert_not person.save, "Saved the person without a first name or last name"
+  end  
 end

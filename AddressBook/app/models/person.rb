c3a051e990f1978c2f489340991c4105a615885d
TomReilly1 <reilly.thomas.c@gmail.com>
2022-12-31 12:21:08 -0500

    define one-to-many relationships from Person model to Address, Email, and PhoneNumber model

--- AddressBook/app/models/person.rb
+++ AddressBook/app/models/person.rb
@@ -1,4 +1,8 @@
 class Person < ApplicationRecord
+    has_many :addresses
+    has_many :emails
+    has_many :phone_numbers
+
     validates :first_name, presence: true
     validates :last_name, presence: true
 end

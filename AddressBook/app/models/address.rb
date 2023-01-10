1051298e155dfaeae6296f87b0374006090a1be0
TomReilly1 <reilly.thomas.c@gmail.com>
2023-01-10 10:46:40 -0500

    add validiation for required fields

--- AddressBook/app/models/address.rb
+++ AddressBook/app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ApplicationRecord
   belongs_to :person
+  validates :street, presence: true
+  validates :town, presence: true
+  validates :zip, presence: true
+  validates :country, presence: true
 end

ca76c6fa4868ba9450bcc7960d88093a7e97eed6
TomReilly1 <reilly.thomas.c@gmail.com>
2023-01-10 12:45:42 -0500

    handle all routes

--- AddressBook/config/routes.rb
+++ AddressBook/config/routes.rb
@@ -1,3 +1,27 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :people do
+    resources :addresses
+    resources :emails
+    resources :phone_numbers
+  end
+  
+  # root "sessions#home"
+  root "people#index"
+
+  # resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
+  # resources :users, only: [:new, :create, :destroy]
+  get '/register', to: 'users#new'
+  resources :users, only: [:create]
+
+  # get "/login", to: "sessions#login"
+  # post "/login", to: "sessions#create"
+  # post "/logout", to: "sessions#destroy"
+  # get "/logout", to: "sessions#destroy"
+
+  get '/login', to: 'sessions#new'
+  post "/login", to: "sessions#create"
+  get '/logout', to: 'sessions#destroy'
+  # resource :sessions, only: [:create]
+
 end
